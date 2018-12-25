e1d7a2afccf0c75b0d5906b155120b7df4e47af3
harp07 <harp07@mail.ru>
2018-12-25 16:02:03 +0400

    refactoring

--- src/main/java/com/javacodegeeks/example/SnmpWalk.java
+++ src/main/java/com/javacodegeeks/example/SnmpWalk.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 import org.snmp4j.CommunityTarget;
 import org.snmp4j.Snmp;
 import org.snmp4j.Target;
@@ -27,10 +29,10 @@ public class SnmpWalk {
     static String IF_MIB_OID=".1.3.6.1.2.1.2.2.1.2";  // ifDescr
     static Set<String> ifset=new HashSet<>();
     static String IP="demo.snmplabs.com";
-    static String[] keyArray;
+    static List<String> keyArray;
     static Map<String, String> walkMap;
     static Map<String, Integer> versionMap=new HashMap<>();
-    static String marked_ports="";
+    static String marked_ports;
     static String snmp_comm="public";
     static String snmp_vers="2";//SnmpConstants.version2c;
     static String snmp_port="161";   
@@ -44,18 +46,22 @@ public class SnmpWalk {
 
     public static void main(String[] args) throws Exception {
         walkMap = walkSNMP(IP, IF_MIB_OID, snmp_comm, snmp_port, snmp_vers); // ifTable, mib-2 interfaces
-        for (Map.Entry<String, String> entry : walkMap.entrySet()) {
-            keyArray=entry.getKey().replace(".","-").split("-"); // entry.getKey() = .1.3.6.1.2.1.2.2.1.2.*
-            ifset.add(keyArray[keyArray.length-1]+":"+entry.getValue());
-            marked_ports=marked_ports+keyArray[keyArray.length-1] +"-";
+        keyArray = walkMap.keySet().stream().map(x->x.replace(".","-").split("-")[11]).collect(Collectors.toList());
+        ifset  = walkMap.entrySet().stream().map(x->x.getKey().replace(".","-").split("-")[11]+":"+x.getValue()).collect(Collectors.toSet());
+        marked_ports=keyArray.stream().map(x->x+"-").reduce((left, right) -> left + right).get();//.collect(Collectors.toList()).toString();
+        //for (Map.Entry<String, String> entry : walkMap.entrySet()) {
+            //keyArray=entry.getKey().replace(".","-").split("-"); // entry.getKey() = .1.3.6.1.2.1.2.2.1.2.*
+            //ifset.add(keyArray[keyArray.length-1]+":"+entry.getValue());
+            //marked_ports=marked_ports+keyArray[keyArray.length-1] +"-";
             //System.out.println(" ifDescr: " + entry.getKey() + " = " + entry.getValue());
             //System.out.println("\n "+keyArray[keyArray.length-1]); 
-        }
+        //}
         System.out.println("\n Walk Map = "+walkMap);
         System.out.println("\n Ports Set = "+ifset);
         System.out.println("\n Last Port Index  = " + ifset.toArray()[ifset.size()-1].toString().split(":")[0]);        
         System.out.println("\n Version Map = "+versionMap);
         System.out.println("\n marked_ports = "+marked_ports);
+        System.out.println("\n keyArray = "+keyArray);
     }
 
     public static Map<String, String> walkSNMP(String ip, String oid, String comm, String port, String vers) throws IOException {
